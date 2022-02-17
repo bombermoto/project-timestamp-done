ae67a63b5ba9331f83a47bf040057677f82a11b2
bombermoto <51003053+bombermoto@users.noreply.github.com>
2022-02-17 11:06:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,26 +18,39 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/:date", function (req, res) {
-  var {date} = req.params;
-  if (date.indexOf("-")>0) {
-    req.time = new Date(date).toUTCString();
-    res.json({
-      unix: Date.parse(date),
-      utc: req.time
-    });
+app.get("/api/:date_string?", function (req, res) {
+  var {date_string} = req.params;
+  let response = {};
+  console.log(req.params);
+//An empty date parameter should return the current time
+  if (req.params.date_string == undefined) {
+    //req.time = new Date().toUTCString();
+    response = {
+        unix: Date.now(),
+        utc: new Date().toUTCString()
+      };
+//for request to /api/1451001600000    
+  } else if (/\d{5,13}/.test(date_string)) {
+      //req.time = new Date(parseInt(date_string, 10)).toUTCString();
+      response = {
+            unix: parseInt(date_string, 10),
+            utc: new Date(parseInt(date_string, 10)).toUTCString()
+        };
+//for any text date requests like "05 October 2011"         
+  } else if (new Date(date_string).toUTCString() != "Invalid Date") {
+      //req.time = new Date(date_string).toUTCString();
+      response = {
+          unix: Date.parse(date_string),
+          utc: new Date(date_string).toUTCString()
+        };
+//if cant read date error        
   } else {
-    req.time = new Date(parseInt(date, 10)).toUTCString();
-    res.json({
-        unix: date,
-        utc: req.time
-    });
+    response = { error : "Invalid Date" };
   }
-   
+  
+  res.json(response);
 });
-
 
 
 // listen for requests :)
